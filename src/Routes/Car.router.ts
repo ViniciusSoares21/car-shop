b33f0ad39f86bccf9daee8444909a0a0464fea0a
Vinicius Soares <viniciussoares72943@gmail.com>
2023-02-13 18:22:49 -0300

    feat: Routers  para buscar carros pelo id e global

--- src/Routes/Car.router.ts
+++ src/Routes/Car.router.ts
@@ -5,4 +5,8 @@ const carRouter = Router();
 
 carRouter.post('/cars', (req, res, next) => new CarController().create(req, res, next));
 
+carRouter.get('/cars', (req, res, next) => new CarController().getAll(req, res, next));
+
+carRouter.get('/cars/:id', (req, res, next) => new CarController().getById(req, res, next));
+
 export default carRouter;
