1b02b202da07445bf1d189a164e4a1c1f422bf54
Vinicius Soares <viniciussoares72943@gmail.com>
2023-02-14 09:21:50 -0300

    refactor: intafeca ICar e Domains Car

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,22 +1,12 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(objct: ICar) {
-    this.id = objct.id;
-    this.model = objct.model;
-    this.year = objct.year;
-    this.color = objct.color;
-    this.status = !!objct.status;
-    this.buyValue = objct.buyValue;
+    super(objct);
     this.doorsQty = objct.doorsQty;
     this.seatsQty = objct.seatsQty;
   }
