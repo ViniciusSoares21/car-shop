bb364bcb2fa7a5546de4f3d8563abc7f207c5d55
Vinicius Soares <viniciussoares72943@gmail.com>
2023-02-14 10:26:02 -0300

    feat: Domai

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,77 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
+
+export default class Motorcycle extends Vehicle {
+  private category: string;
+  private engineCapacity: number;
+
+  constructor(objct: IMotorcycle) {
+    super(objct);
+    this.category = objct.category;
+    this.engineCapacity = objct.engineCapacity;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setCategory(category: string) {
+    this.category = category;
+  }
+
+  public getCategory() {
+    return this.category;
+  }
+
+  public setEngineCapacity(value: number) {
+    this.engineCapacity = value;
+  }
+
+  public getEngineCapacity() {
+    return this.engineCapacity;
+  }
+}
